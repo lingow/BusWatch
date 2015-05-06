414fe863e73dbea665ec92c9961c674162ba8968
lingow <elingow@gmail.com>
2015-05-06 01:40:26 -0500

    Adding Jetty Server

--- buswatchserver/src/main/java/com/lingoware/lingow/buswatch/server/BusWatchServer.java
+++ buswatchserver/src/main/java/com/lingoware/lingow/buswatch/server/BusWatchServer.java
@@ -1,10 +1,18 @@
 package com.lingoware.lingow.buswatch.server;
 
+import org.eclipse.jetty.server.Server;
+
 import javax.xml.ws.Endpoint;
 
 public class BusWatchServer {
 
-    public static void main(String args[]) {
-        Endpoint.publish("http://localhost:" + System.getenv("PORT") + "/", new BusWatchServiceImplementor());
+    public static void main(String args[]) throws Exception {
+        String port = System.getenv("PORT");
+
+        Server server = new Server(Integer.parseInt(port));
+
+        Endpoint.publish("http://localhost:" + port + "/", new BusWatchServiceImplementor());
+        server.start();
+        server.join();
     }
 }
