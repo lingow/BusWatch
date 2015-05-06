e87d1f757760e892e481da30290a8c8d4205e8e5
lingow <elingow@gmail.com>
2015-05-06 02:54:24 -0500

    Fixing jetty server

--- buswatchserver/src/main/java/com/lingoware/lingow/buswatch/server/BusWatchServer.java
+++ buswatchserver/src/main/java/com/lingoware/lingow/buswatch/server/BusWatchServer.java
@@ -1,18 +1,27 @@
 package com.lingoware.lingow.buswatch.server;
 
+import org.eclipse.jetty.http.spi.JettyHttpServer;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 
 import javax.xml.ws.Endpoint;
 
 public class BusWatchServer {
 
     public static void main(String args[]) throws Exception {
-        String port = System.getenv("PORT");
+        int port = Integer.parseInt(System.getenv("PORT"));
+        System.out.println("Puerto " + port);
 
-        Server server = new Server(Integer.parseInt(port));
+        System.setProperty("com.sun.net.httpserver.HttpServerProvider", JettyHttpServer.class.getName());
 
-        Endpoint.publish("http://localhost:" + port + "/", new BusWatchServiceImplementor());
+        Server server = new Server(port);
+        JettyHttpServer jettyServer = new JettyHttpServer(server, true);
+        ContextHandlerCollection collection = new ContextHandlerCollection();
+        server.setHandler(collection);
+        Endpoint endpoint = Endpoint.create(new BusWatchServiceImplementor());
+        endpoint.publish(jettyServer.createContext("/"));
         server.start();
         server.join();
+
     }
 }
